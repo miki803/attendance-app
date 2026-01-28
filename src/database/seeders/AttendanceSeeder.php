004946bbb95075b3ecb042595bb6851d79dac99c
miki803 <zg0shyu@gmail.com>
2026-01-28 17:00:33 +0900

    Seederにより、一般ユーザーの1か月分の勤怠データ（出勤日・欠勤日・未退勤日・休憩あり/なし）を自動生成している。

--- src/database/seeders/AttendanceSeeder.php
+++ src/database/seeders/AttendanceSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use App\Models\Attendance;
 use App\Models\User;
+use App\Models\BreakTime;
+use Carbon\Carbon;
 
 class AttendanceSeeder extends Seeder
 {
@@ -17,13 +19,55 @@ class AttendanceSeeder extends Seeder
     {
         //ユーザーを1人取得（なければ作る）
         //「勤怠データが1件もないと画面確認できないから、テスト用に attendance を1件 DB に入れる」
-        $user = User::first() ?? User::factory()->create();
-        Attendance::create([
-            'user_id' => $user->id,
-            'date' => now()->toDateString(),
-            'start_time' => '09:00',
-            'end_time' => '18:00',
-            'status' => 'finished',
-        ]);
+        $users = User::where('is_admin', false)->get();
+        // 今月
+        $start = Carbon::now()->startOfMonth();
+        $end   = Carbon::now()->endOfMonth();
+        foreach ($users as $user) {
+            $date = $start->copy();
+            while ($date <= $end){
+                // 土日は休みにする
+                if ($date->isWeekend()) {
+                    $date->addDay();
+                    continue;
+                }
+                // ランダム判定
+                $rand = rand(1, 100);
+
+                //欠勤（20%）
+                if ($rand <= 20) {
+                    $date->addDay();
+                    continue;
+                }
+                //未退勤（10%）
+                if ($rand <= 30) {
+                    Attendance::create([
+                        'user_id' => $user->id,
+                        'date' => $date->toDateString(),
+                        'start_time' => '09:00',
+                        'end_time' => null,
+                        'status' => 'working',
+                    ]);
+                    $date->addDay();
+                    continue;
+                }
+                //通常出勤（70%）
+                $attendance = Attendance::create([
+                    'user_id' => $user->id,
+                    'date' => $date->toDateString(),
+                    'start_time' => '09:00',
+                    'end_time' => '18:00',
+                    'status' => 'finished',
+                ]);
+                if (rand(1, 100) <= 80) {
+                    BreakTime::create([
+                        'attendance_id' => $attendance->id,
+                        'start_time' => '12:00',
+                        'end_time' => '13:00',
+                    ]);
+                }
+                $date->addDay();
+            }
+        }
     }
 }
